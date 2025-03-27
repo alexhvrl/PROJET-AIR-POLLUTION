170d93e3d1b47115f82c2baea2d8c884f9c57469
Yoyo-cloud-byte <yoan.cangemi@epfl.ch>
2025-03-27 12:50:27 +0100

    fhcfgg

--- question4.py
+++ question4.py
@@ -2,3 +2,8 @@ print('qu'est'ce qu'il va se passer')
 print("slaut")
 print("i")
 print("sindu")
+
+ git config --global user.email "yoan.cangemi@epfl.ch"
+  git config --global user.name "Yoyo-cloud-byte"
+
+  print("ca marche")
