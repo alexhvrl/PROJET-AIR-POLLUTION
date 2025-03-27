96fd4f97c224d67c8e146a7b796d92e25b8313d5
alexhvrl <alexandre.hivorel@gmail.com>
2025-03-27 14:13:08 +0100

    changement j'enlève les trucs

--- question4.py
+++ question4.py
@@ -1,9 +1,6 @@
-print('qu'est'ce qu'il va se passer')
+print("qu'est'ce qu'il va se passer")
 print("slaut")
 print("i")
 print("sindu")
-
- git config --global user.email "yoan.cangemi@epfl.ch"
-  git config --global user.name "Yoyo-cloud-byte"
 
   print("ca marche")
